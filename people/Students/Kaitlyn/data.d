3d5a1e63d2003fc6babb4ceae0bcc564086a7189
Nguy <klnguy@ieng6-201.ucsd.edu>
2025-07-09 15:40:05 -0700

    Edited data.md x3

---
+++ people/Students/Kaitlyn/data.d
@@ -0,0 +1,10 @@
+Name: Kaitlyn
+Role: Student
+Team: 2
+Year at UCSD in fall: 1
+
+Favorite CSE29 topic: Binary
+Favorite UCSD class: CSE 29
+
+Favorite food: Korean Fried Chicken
+Favorite drink: Apple Milkis or Matcha Latte
---
+++ people/Students/Kaitlyn/data.md
@@ -0,0 +1,10 @@
+Name: kaitlyn nguy
+Role: student
+Team: 2
+Year at UCSD in fall: 1
+
+Favorite CSE29 topic: binary
+Favorite UCSD class: cse 29
+
+Favorite food: korean fried chicken
+Favorite drink: apple milkis
